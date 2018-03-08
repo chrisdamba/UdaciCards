3224ba7a55980df8beef2bd0a54ec4b2268131dd
Christopher Dambamuromo <chridam@gmail.com>
2018-03-08 09:05:25 +0000

    Added reducer

--- models/Card.js
+++ models/Card.js
@@ -1,18 +1,22 @@
 import { guid } from '../utils'
 
 class Card {
-  constructor(deckID) {
-    this.deckID = deckID
+  constructor(deckId, questions, title) {
+    this.deckID = deckId
+    this.questions = questions
+    this.title = title
     this.id = guid()
   }
 
   setFromObject(ob) {
-    this.deckID = ob.deckID
+    this.deckId = ob.deckId
+    this.questions = ob.questions
+    this.title = ob.title
     this.id = ob.id
   }
 
   static fromObject(ob) {
-    let c = new Card(ob.deckID)
+    let c = new Card(ob.deckId, ob.questions, ob.title)
     c.setFromObject(ob)
     return c
   }
